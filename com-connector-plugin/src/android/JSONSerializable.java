ed06e337c63c1b7fc89ee57b7549b8ea0baa2ba3
Hubert <dominicus_hubert@icloud.com>
2016-02-04 21:04:18 +1100

    Test remove cached

--- com-connector-plugin/src/android/JSONSerializable.java
+++
@@ -1,9 +0,0 @@
-package com.contactpoint.plugin;
-
-import org.json.JSONObject;
-
-public interface JSONSerializable {
-
-	public void setAttributes(JSONObject jsonObject);
-	public JSONObject toJSONObject();
-}
