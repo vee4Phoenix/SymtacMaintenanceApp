70168a101ff030c674cd76df688e74c2305291b5
Hubert <dominicus_hubert@icloud.com>
2016-05-14 19:15:54 +1000

    v0.0.7

--- www/model/constants.js
+++ www/model/constants.js
@@ -19,17 +19,17 @@
       'Problem' : 2,
       
       // Equipment type
-      'BREAK'        : 1,
-      'DRY_EXT'      : 2,
-      'PHONE'        : 3,
-      'EXIT'         : 4,
-      'FIRE_HOSE'    : 5,
-      'FIRST_AID'    : 6,
-      'SPANNER'      : 7,
-      'EWIS'         : 8,
-      'FIP'          : 9,
-      'FIRE_BLANKET' : 10,
-      'SWITCHBOARD'  : 11
+      'BREAK'        : '1',
+      'DRY_EXT'      : '2',
+      'PHONE'        : '3',
+      'EXIT'         : '4',
+      'FIRE_HOSE'    : '5',
+      'FIRST_AID'    : '6',
+      'SPANNER'      : '7',
+      'EWIS'         : '8',
+      'FIP'          : '9',
+      'FIRE_BLANKET' : '10',
+      'SWITCHBOARD'  : '11'
       
     });
 
